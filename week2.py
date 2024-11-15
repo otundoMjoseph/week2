519506fda81f87348c43e35dd69aece469d7c11a
otundoMjoseph <josephmainaotundo@gmail.com>
2024-11-15 17:40:57 +0300

    Finished all questions

---
+++ week2.py
@@ -0,0 +1,24 @@
+#question 1
+my_list = []
+# my_filled_list = [10, 20, 30, 40]
+my_list.extend([10, 20, 30, 40])
+
+#question 2
+my_list.insert(2, 15)
+
+
+#question 3
+my_list.extend([50, 60, 70])
+
+
+#question 4
+my_list.pop()
+#question5
+my_list.sort()
+
+index_thirty = my_list.index(30)
+print(index_thirty)
+print(my_list)
+
+
+
